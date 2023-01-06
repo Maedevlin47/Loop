be35a116f6eac79dd450e790935cac549c10b083
Mae Schreck <maedevlin47@gmail.comm>
2023-01-06 01:01:10 -0500

    logout hide -helped

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-export default function NavBar() {
+export default function NavBar({user}) {
     return(
         <div>
-            <NavLink className="login" to="/login">Login</NavLink>
-            {/* <NavLink className="home" to="/">Home</NavLink> */}
-            <NavLink className="signup" to="/signup">Signup</NavLink>
+        {user?
+                
+            (
+            <div className="loggedin">
             <NavLink className="events" to="/events">Event List</NavLink>
             <NavLink className="logout" to="/logout">Logout</NavLink>
+            </div>
+            ) : (
+            <div className="loggedout">
+            <NavLink className="login" to="/login">Login</NavLink>
+            <NavLink className="signup" to="/signup">Signup</NavLink>
+            </div>
+            )
+        }
         </div>
     )
 }
